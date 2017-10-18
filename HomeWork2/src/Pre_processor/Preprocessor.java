891d321fb26d908edf38e9ac3dacd1ec303dd179
Daniel <daniel_1992@naver.com>
2017-10-19 01:11:42 +0900

    웹프레임워크 2주차 pre-processor 해결

---
+++ HomeWork2/src/Pre_processor/Preprocessor.java
@@ -0,0 +1,26 @@
+package Pre_processor;
+
+public class Preprocessor {
+    static final boolean DEBUG=true;
+    public static void main(String[] args) {
+
+
+
+        if(true)
+        {
+            if(DEBUG)
+            {
+                System.out.println("나온다.");
+            }
+        }
+        if(false)
+        {
+            if(DEBUG)
+            {
+                System.out.println("이건나올까?");
+            }
+        }
+
+
+    }
+}
